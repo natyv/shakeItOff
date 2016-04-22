c2e0aca35db6fdc4082e19decf75b11bcf893adf
Pranati Dave <pranati.dave@gmail.com>
2016-04-22 15:32:54 +1000

    db hook

--- db_config.rb
+++ db_config.rb
@@ -4,5 +4,4 @@ options = {
   adapter: 'postgresql',
   database: 'shakeitoff'
 }
-
-ActiveRecord::Base.establish_connection(options)
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || options)`
